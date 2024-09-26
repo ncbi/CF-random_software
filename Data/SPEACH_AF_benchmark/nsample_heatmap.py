1cf15b4236547155f4d7f516c089780880d815a0
mslee1220 <99306709+mslee1220@users.noreply.github.com>
2024-09-26 12:51:24 -0400

    Add files via upload

--- Data/SPEACH_AF_benchmark/nsample_heatmap.py
+++ Data/SPEACH_AF_benchmark/nsample_heatmap.py
@@ -7,6 +7,7 @@ Created on Mon Sep 16 13:00:00 2024
 """
 import os
 import sys
+import csv
 import textalloc as ta
 from pathlib import Path
 import numpy as np
@@ -19,29 +20,53 @@ import glob
 
 
 
-nsample = genfromtxt("number_of_predictions.csv", delimiter = ' ')
 
-ref_Mc = sorted(glob.glob("ref_data_Mchaourab*csv"))
 
-#### printing name
-ref_name = []
-for name in ref_Mc:
-    ref_Mc_name = name.replace('.csv', '')
-    ref_Mc_name = ref_Mc_name.replace('ref_data_Mchaourab_', '')
+data_name = open('list_of_OC23-uniprot_ID.csv', 'r')
+myreader = csv.reader(data_name)
 
-    ref_name = np.append(ref_name, ref_Mc_name)
+uniprot_name = []; pdb1_name = []; pdb2_name = []; nsampleO = []; nsampleC = []
+
+for row in myreader:
+    uniprot_name = np.append(uniprot_name, row[0])
+    pdb1_name = np.append(pdb1_name, row[1])
+    pdb2_name = np.append(pdb2_name, row[2])
+    nsampleO = np.append(nsampleO, row[3])
+    nsampleC = np.append(nsampleC, row[4])
+    
+uniprot_name = uniprot_name[1:]
+print(uniprot_name)
+pdb1_name = pdb1_name[1:]
+pdb2_name = pdb2_name[1:]
+nsampleO = nsampleO[1:]
+nsampleC = nsampleC[1:]
+uniprot_size = np.size(uniprot_name)
+
+
+pdb1_name = pdb1_name.ravel().astype(str)
+pdb2_name = pdb2_name.ravel().astype(str)
+#uniprot_name = uniprot_name.ravel().astype(list)
+nsampleO = nsampleO.ravel().astype(int)
+nsampleC = nsampleC.ravel().astype(int)
+
+
+
+pdb1_name = pdb1_name.reshape(uniprot_size, 1); pdb2_name = pdb2_name.reshape(uniprot_size, 1)
+nsampleO = nsampleO.reshape(uniprot_size, 1); nsampleC = nsampleC.reshape(uniprot_size, 1);
+
+nsample_all = np.concatenate((nsampleO, nsampleC), axis = 1)
 
 
 
 # plotting the heatmap
-hm = sns.heatmap(data=nsample,
-                annot=True, vmin=125, vmax=700, cmap="rocket_r", fmt="3.0f")
+hm = sns.heatmap(data=nsample_all,
+                annot=True, vmin=125, vmax=1000, cmap="rocket_r", fmt="3.0f")
 hm.figure.axes[-1].set_ylabel('Number of samples', size=13, rotation = 270, labelpad = 15)
-y_values = np.arange(0.5, 14.5, 1)
+y_values = np.arange(0.5, int(uniprot_size) + 0.5, 1)
 x_values = np.arange(0.5, 2.5, 1)
 #x_label = ['SPEACH F1', 'SPEACH F2', 'CF Fold1', 'CF Fold2']
-x_label = ['SPEACH-AF', 'CF-random']
-plt.yticks(y_values, ref_name)
+x_label = ['AFsample2', 'CF-random']
+plt.yticks(y_values, uniprot_name)
 plt.yticks(rotation=0)
 plt.xticks(x_values, x_label)
 plt.tick_params(
@@ -49,5 +74,5 @@ plt.tick_params(
     bottom=False,      # ticks along the bottom edge are off
     top=False)         # ticks along the top edge are off
 
-plt.savefig('heatmap-nsamples.png',dpi=600)
+plt.savefig('OC23_heatmap-nsamples.png',dpi=600)
 
