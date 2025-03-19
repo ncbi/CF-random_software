04c2aec41791ccbb03ef673bececabbaf04a86f5
Myeongsang (Samuel) Lee <99306709+mslee1220@users.noreply.github.com>
2025-03-19 15:32:19 -0400

    Update search_w_foldseek_cluster.py

--- codes/search_w_foldseek_cluster.py
+++ codes/search_w_foldseek_cluster.py
@@ -217,7 +217,7 @@ class blind_screening():
             print("removed from analysis: ",file.replace("-self.foldseek",".pdb"))
         files = files[~mask]
         files = sorted(files)
-        files_pdb = [file.replace('/','-')[2:].replace("-self.foldseek",".pdb") for file in files]
+        files_pdb = [file.replace('/','-')[3:].replace("-self.foldseek",".pdb") for file in files]
         corr_mtx = []
     
         df = {}
